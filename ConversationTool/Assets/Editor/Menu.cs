70309c466953a621ee3ecff8b9241126cde54732
MoCastle <18224837254@163.com>
2019-05-28 08:51:49 +0800

    afdas

---
+++ ConversationTool/Assets/Editor/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Menu:EditorWindow {
+    [MenuItem("EditorTool/ConversationEditor")]
+    static public void init()
+    {
+        GetWindow(typeof(ToolEditorWindow), false, "ToolEditorWindow");
+    }
+}
